b8990727c951327789a40a9d6d8272b3515009d8
Seonuk Kim <sukim@Seonukui-MacBookAir.local>
2026-06-09 19:21:19 +0900

    Remove unnecessary startup handler

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onStartup.addListener(() => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    if (tabs[0]) {
-      chrome.tabs.update(tabs[0].id, { url: chrome.runtime.getURL("newtab.html") });
-    }
-  });
-});
